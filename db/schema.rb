b3c2d775fc582caf82c81ef02defdf943755b22e
elysanskiy <elysanskiy@phoenixit.ru>
2022-06-21 17:11:58 +0300

    Added messages model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_21_135103) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_21_140828) do
   create_table "messages", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "body"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
