a85220b4a090db94b384d09f3f22ec702b5ba3f5
elysanskiy <elysanskiy@phoenixit.ru>
2022-06-23 15:31:18 +0300

    Added create messages to UI

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,11 @@ class ApplicationController < ActionController::Base
   def logged_in?
     !!current_user
   end
+
+  def require_user
+    unless logged_in?
+      flash[:error] = "You must be logged in"
+      redirect_to login_path
+    end
+  end
 end
