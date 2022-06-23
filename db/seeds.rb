3491192dfcb27fad2c541aed263a975f24b4d6a4
Keallar <zlysanskiy@gamil.com>
2022-06-23 12:19:17 +0300

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,13 @@
+# create admin
+User.create(username: "admin", password: "qwerty$4")
+
+# create users
 5.times do |i|
     User.create(username: "user_#{i}", password: "password")
+end
+
+# create messages
+5.times do |i|
+    user = User.all[i]
+    Message.create("Something new from #{user.username}", user_id: user.id)
 end
